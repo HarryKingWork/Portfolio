412744fbddb1eed27ac5ec991560fd104bbd91a4
nicholas harper <nicholasharper000@gmail.com>
2024-12-17 19:31:34 -0800

    Update server.js

--- server.js
+++ server.js
@@ -1,17 +1,15 @@
 const https = require('https');
 const fs = require('fs');
 const next = require('next');
-
 const dev = process.env.NODE_ENV !== 'production';
 const app = next({ dev });
 const handle = app.getRequestHandler();
 
 app.prepare().then(() => {
-  // Use relative paths for your SSL certificates
   const options = {
     key: fs.readFileSync('./certification/private.key'),
     cert: fs.readFileSync('./certification/certificate.crt'),
-    ca: fs.readFileSync('./certification/ca_bundle.crt') // Optional
+    ca: fs.readFileSync('./certification/ca_bundle.crt')
   };
 
   const server = https.createServer(options, (req, res) => {
